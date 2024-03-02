92d128d299e36ea8936c1004bf6805135d624dd6
alazaradane <alazargithub369@gmail.com>
2024-03-02 21:08:31 +0300

    subscribe section updated

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -6,10 +6,10 @@ const Subscribe = () => {
     <section
       id='contact-us'
       className=' max-container flex justify-between items-center flex-col gap-10'>
-          <h3 className=' text-4xl leading-[68px]  font-palanquin font-bold'>Sign Up For <span className=' text-coral-red'>Updates</span> & Newsletter</h3>
-          <div className=' max-lg:w-1/2 max-sm:w-full w-[80%] flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full max-md:w-[30rem]'>
-            <input type="text" className=' input bg-black border-2 focus:border-coral-blue' placeholder='Subscribe@smartbrain.com'/>
-            <div className=' flex max-sm:justify-end items-center max-sm:w-[30%]'>
+          <h3 className=' text-4xl leading-[68px]  font-palanquin font-bold'>Sign Up For <span className=' text-coral-blue'>Updates</span> & Newsletter</h3>
+          <div className=' max-lg:w-1/2 max-sm:w-full w-[80%] flex items-center justify-between max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full max-md:w-[30rem]'>
+            <input type="text" className=' input placeholder:px-4 py-3 focus:border-none focus:outline-none w-full  focus:border-coral-blue' placeholder='Subscribe@stemdl369.com'/>
+            <div className=' flex max-sm:justify-end items-center max-sm:w-[30%] w-[20%]'>
               <Button label={'Sign Up'} fullwidth/>
             </div>
           </div>
